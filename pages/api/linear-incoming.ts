2da22f57bba623ca9dd1f61879af720e3be64234
Adam Kaczmarek <adam@loops.so>
2022-12-16 14:57:25 -0500

    add linear connection and look for open children

--- pages/api/linear-incoming.ts
+++ pages/api/linear-incoming.ts
@@ -1,14 +1,48 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { LinearClient } from "@linear/sdk";
+
+// Api key authentication
+const linear = new LinearClient({
+  apiKey: process.env.LINEAR_KEY,
+});
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log(req.body);
-  res.status(200).json({ name: "John Doe" });
+  const isUpdate = req.body.action === "update";
+  const isComplete = req.body.data.state.type === "completed";
+  const wasPreviouslyNotComplete = req.body.updatedFrom.completedAt === null;
+
+  if (!(isUpdate && isComplete && wasPreviouslyNotComplete)) {
+    res.status(200).end();
+    return;
+  }
+  const issueId = req.body.data.id;
+  // todo - is there a way to filter issues by parent
+  const issues = await linear.issues({
+    filter: {
+      id: { eq: issueId },
+    },
+  });
+  const issue = issues.nodes[0];
+  const issueChildren = await issue.children({
+    filter: {
+      state: {
+        type: { neq: "completed" },
+      },
+    },
+  });
+  if (issueChildren.nodes.length) {
+    issueChildren.nodes.forEach((element) => {
+      console.log(`FOLLOW UP REQUIRED for ${element.identifier}`);
+    });
+  }
+  res.status(200).end();
+  return;
 }
